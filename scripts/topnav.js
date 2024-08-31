c95d7faf41a05bd2b8a2f0faff42fa9f050cbce8
zultymarcin <152528508+zultymarcin@users.noreply.github.com>
2024-08-31 19:40:03 +0200

    responsive navigation done

--- scripts/topnav.js
+++ scripts/topnav.js
@@ -1,9 +1,13 @@
  function miniNav() {
     var navStatus = document.getElementById("myTopnav");
+    var icon = document.querySelector(".icon i");
+
     if(navStatus.className === "topnav") {
         navStatus.className += " responsive";
+        icon.className = "fa fa-times";
     } else {
         navStatus.className = "topnav";
+        icon.className = "fa fa-bars";
     }
 
  }
